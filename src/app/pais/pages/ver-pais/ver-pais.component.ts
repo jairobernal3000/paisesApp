bb77f7b41b8235adbc18b297592527bf7f0555b3
jairobernal3000 <jairobernal3000@gmail.com>
2021-07-10 23:13:45 -0500

    Initial commit

---
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-pais',
+  templateUrl: './ver-pais.component.html',
+  styles: [
+  ]
+})
+export class VerPaisComponent {
+
+  constructor() { }
+
+
+}
